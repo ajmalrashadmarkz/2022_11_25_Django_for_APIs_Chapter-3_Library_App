86682ab8c44dd6dd9922b06241867789e5bfc74e
rashadmarkz <rashad.markcreative.com>
2022-12-02 12:37:28 +0530

    Create Model class and super user

--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import  Book
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Book
     template_name = "home.html"
